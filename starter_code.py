d7c75c799b5bfe367386359e380b7b15129d10ac
ywatnabe <watanabe111016@icloud.com>
2025-03-08 16:57:29 -0800

    add sos in matlab

--- starter_code.py
+++ starter_code.py
@@ -1,6 +1,5 @@
 from utils.data_utils import generate_data, kitti_data, construct_data_matrix
 from core.solvers import riemannian_gradient_descent, projected_gradient_descent, sequential_qp, multi_search_wrapper
-from core.solvers import original_qcqp, SDP_relaxation, dual_QCQP
 import numpy as np
 
 
@@ -15,11 +14,8 @@ if __name__ == '__main__':
     list_P, list_Omega = construct_data_matrix([obsv_z[:, :, None]], [lmark_m[:, :, None]])
 
     # Define and solve the PnP problem
-    # R_sol_sqp = sequential_qp(Omega=list_Omega[0], R_init=np.eye(3), max_steps=1000)
-    # R_sol = multi_search_wrapper(Omega=list_Omega[0], solver=riemannian_gradient_descent, max_steps=500)
-    R_sol = SDP_relaxation(Omega=list_Omega[0], R_init=np.eye(3))
-    gamma = dual_QCQP(Omega=list_Omega[0], R_init=np.eye(3))
+    # R_sol = sequential_qp(Omega=list_Omega[0], R_init=np.eye(3), max_steps=1000)
+    R_sol = multi_search_wrapper(Omega=list_Omega[0], solver=riemannian_gradient_descent, max_steps=500)
 
     print("Difference from GT: ", np.linalg.norm(gt_list[0] - R_sol))
-    print("SDP relaxation cost: ", R_sol.flatten().T @ list_Omega[0] @ R_sol.flatten().T)
-    print("Dual prolmen report lower bound: ", gamma.squeeze())
+    print(R_sol)
